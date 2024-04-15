ba98eac3d758c07ebef79efab7ecb68325fddefe
Jayden Fassett <fassettjayden@gmail.com>
2024-04-15 16:35:58 -0400

    commit

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,18 +1,34 @@
-from towhee import pipe
+from towhee import pipe,ops,DataCollection
 import torchvision
-
+from towhee.trainer.training_config import dump_default_yaml, TrainingConfig
+from towhee.trainer.trainer import Trainer
+from dataset import NFLDataset
+import pathlib
 
 #Takes video file & metadata
 
-preprocess = (
+ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='video', device='cuda')
+model = (
     pipe.input('vid','text')
-        .map('vid', 'y', lambda x: x + 1)
-        .map('text', 'y', lambda x: x + 1)
-        .output('y')
+        .map('text', 'tvec', ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='text', device='cuda')) \
+        # .map('vid', 'vvec', ) \
+        .output('vid')
 )
 
 
-training_configs = TrainingConfig(
-     xxx='some_value_xxx',
-     yyy='some_value_yyy'
+training_config = TrainingConfig(
+    output_dir="../models/train_model",
+    epoch_num=2,
+    batch_size=1,
+    print_steps=1,
+
 )
+
+if __name__ == "__main__":
+    which = "train"
+    data_dir = pathlib.Path(__file__).parents[1] / "data"
+    train_data = NFLDataset(data_dir/which,True)
+    which = "val"
+    eval_data = NFLDataset(data_dir/which,True)
+
+    trainer = Trainer(model, training_config, train_dataset=train_data, eval_dataset=eval_data)
