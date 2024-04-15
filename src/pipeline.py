c75d69a35843db323e392f78e23ef81e1375ad06
jamorr <55964176+jamorr@users.noreply.github.com>
2024-04-15 18:15:01 -0400

    Add clip4clip support

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,9 +4,25 @@ import torchvision
 from towhee.trainer.training_config import dump_default_yaml, TrainingConfig
 from towhee.trainer.trainer import Trainer
 from dataset import NFLDataset
-#Takes video file & metadata
+from towhee.models.clip4clip import CLIP4Clip, create_model
 
-model = ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='video').get_op()
+# model = create_model()
+#Takes video file & metadata
+model = CLIP4Clip(**dict(
+        # vision
+        embed_dim=512,
+        image_resolution=224,
+        vision_layers=12,
+        vision_width=768,
+        vision_patch_size=32,
+        # text
+        context_length=77,
+        vocab_size=49408,
+        transformer_width=512,
+        transformer_heads=8,
+        transformer_layers=12
+    ))
+# model = ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='video', device='cpu').get_op()
 # model = (
 #     pipe.input('vid','text')
 #         .map('text', 'tvec', ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='text', device='cuda')) \
@@ -16,7 +32,7 @@ model = ops.video_text_embedding.clip4clip(model_name='clip_vit_b32', modality='
 
 
 training_config = TrainingConfig(
-    output_dir="../models/train_model",
+    output_dir="../models",
     epoch_num=2,
     batch_size=1,
     print_steps=1,
@@ -33,7 +49,7 @@ if __name__ == "__main__":
     trainer = Trainer(model, training_config, train_dataset=train_data, eval_dataset=eval_data)
     # data_dir = pathlib.Path(__file__).parents[1] / "data" / "val"
     # dset = NFLDataset(data_dir, True)
-    
+
     trainer.train()
     # for i in range(len(dset))[:10]:
     #     model(dset[i][0])
