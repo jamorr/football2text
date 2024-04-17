83f148d25f82f04a789c4bcf0f3b37450b9659e9
jamorr <Morris.Jack@outlook.com>
2024-04-17 19:28:29 -0400

    Formatting fix for dataset clip

--- src/CLIP/dataset.py
+++ src/CLIP/dataset.py
@@ -1,35 +1,31 @@
 import pathlib
-import shutil
 from typing import Any
-from PIL import Image as PILImage
 
-import numpy as np
 import pandas as pd
-import torch
-from lightning.pytorch import LightningDataModule, seed_everything
-from torch.utils.data import DataLoader, Dataset
-from torchvision.io import read_video, read_image
-from towhee import ops
-from towhee.models.clip.clip_utils import tokenize
-from torchvision import transforms
-from torchvision.io import write_jpeg, write_png
-from transformers import AutoImageProcessor, ViTMAEModel
-from tqdm import tqdm
-from transformers import AutoTokenizer
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+from transformers import AutoImageProcessor
+
 
 class NFLJPEGDataset(Dataset):
-    def __init__(self, data_path:pathlib.Path, which:str = "train") -> None:
+    def __init__(self, data_path: pathlib.Path, which: str = "train") -> None:
         super().__init__()
         if isinstance(data_path, str):
             data_path = pathlib.Path(data_path)
         assert data_path.exists()
-        #Img
-        self.data_dir:pathlib.Path = data_path
-        self.img_path:pathlib.Path = data_path / "jpeg_data"
+        # Img
+        self.data_dir: pathlib.Path = data_path
+        self.img_path: pathlib.Path = data_path / "jpeg_data"
         self.img_list = list(self.img_path.glob("*.jpeg"))
-        self.image_processor = AutoImageProcessor.from_pretrained("facebook/vit-mae-base")
-        #Text
-        self.target = pd.read_parquet(data_path/ which / "target.parquet", dtype_backend='numpy_nullable', columns=['gameId', 'playId','playDescription']) #.convert_dtypes(dtype_backend='numpy_nullable')
+        self.image_processor = AutoImageProcessor.from_pretrained(
+            "facebook/vit-mae-base"
+        )
+        # Text
+        self.target = pd.read_parquet(
+            data_path / which / "target.parquet",
+            dtype_backend="numpy_nullable",
+            columns=["gameId", "playId", "playDescription"],
+        )  # .convert_dtypes(dtype_backend='numpy_nullable')
         self.which = which
         #     self.tfms = transforms.Compose([
         #     transforms.ToPILImage(),
@@ -39,22 +35,25 @@ class NFLJPEGDataset(Dataset):
         #     transforms.Normalize(
         #         (0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
         # ])
+
     def __len__(self):
         return len(self.img_list)
 
     def __getitem__(self, index) -> Any:
         img_file_path = self.img_list[index]
         file_name = img_file_path.name
-        gameId, playId, frameID = file_name.split('-')[0:2]
-        frameID = frameID[:-5] # Removing .jpeg
+        gameId, playId, frameID = file_name.split("-")[0:2]
+        frameID = frameID[:-5]  # Removing .jpeg
 
         # Getting play, and then indexing by frameID. If this doesn't work then we might need to reset_index
-        self.play = self.target[(self.target["gameId"] == gameId)&(self.target["playId"] == playId)]
-        text = self.play.iloc[frameID]['playDescription']
+        self.play = self.target[
+            (self.target["gameId"] == gameId) & (self.target["playId"] == playId)
+        ]
+        text = self.play.iloc[frameID]["playDescription"]
         return text, read_image(img_file_path)
         # THIS DOESNT WORK FOR SOME REASON
         # return read_image(str(self.img_list[index % len(self)].absolute()))
-    
+
 
 if __name__ == "__main__":
     data_dir = pathlib.Path(__file__).parents[1].parent / "data"
